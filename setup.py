9868899b6d8cc873e86e26eea44b5c557f9171f7
Jesse <jesse@jessejanssen.nl>
2024-04-22 12:02:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Jesse Janssen',
     url='https://github.com/69Jesse/PyHTSL',
     version='1.0.4',
-    packages=find_packages(),
+    packages=find_packages() + ['pyhtsl.misc'],
     description='Python wrapper for HTSL created to simplify the process of making housings on Hypixel',
     python_requires='>=3.12',
     package_data={'': ['*.*']},
