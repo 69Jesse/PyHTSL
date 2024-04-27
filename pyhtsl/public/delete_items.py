fb1f3e22b4e6c9f4c85907569f71e8852328f978
Jesse <jesse@jessejanssen.nl>
2024-04-27 06:19:32 +0200

    better debug msg

--- pyhtsl/public/delete_items.py
+++ pyhtsl/public/delete_items.py
@@ -15,4 +15,4 @@ def delete_all_items_from_imports_folder() -> None:
         if not path.name.startswith('_'):
             continue
         path.unlink()
-        print(f'Deleted "{path.absolute()}"')
+        print(f'\x1b[38;2;255;0;0mDeleted the Item .json file at the following location:\x1b[0m\n{path.absolute()}')
