c113653cae975bdf392b9ee68c6bada364899b5c
Jesse <jesse@jessejanssen.nl>
2024-08-20 19:12:48 +0200

    Possibility to add None as a team for RequiredTeam

--- pyhtsl/public/required_team.py
+++ pyhtsl/public/required_team.py
@@ -1,7 +1,7 @@
 from ..condition import TinyCondition
 from .team import Team
 
-from typing import final
+from typing import Optional, final
 
 
 __all__ = (
@@ -11,12 +11,12 @@ __all__ = (
 
 @final
 class RequiredTeam(TinyCondition):
-    team: Team
+    team: Optional[Team]
     def __init__(
         self,
-        team: Team | str,
+        team: Optional[Team | str],
     ) -> None:
-        self.team = team if isinstance(team, Team) else Team(team)
+        self.team = team if not isinstance(team, str) else Team(team)
 
     def __str__(self) -> str:
-        return f'hasTeam "{self.team.name}"'
+        return f'hasTeam "{self.team.name if self.team is not None else "None"}"'
