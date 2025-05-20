e23ed49d816f75ee0a6b7bf3035b748f4026ecc1
Jesse <jesse@jessejanssen.nl>
2025-05-20 19:22:27 +0200

    Another small fix

--- pyhtsl/public/_inside_line.py
+++ pyhtsl/public/_inside_line.py
@@ -1,5 +1,6 @@
 from ..checkable import Checkable
 from ..expression.housing_type import HousingType
+from ..expression.handler import EXPR_HANDLER
 
 
 __all__ = (
@@ -8,6 +9,7 @@ __all__ = (
 
 
 def _inside_line(value: Checkable | HousingType) -> str:
+    EXPR_HANDLER.push()
     if isinstance(value, HousingType):
         return str(value)
     return value._in_assignment_right_side()
