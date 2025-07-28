013f9e2d732a0c8e78a67e1d20aa95a32e91f117
Jesse <jesse@jessejanssen.nl>
2025-07-28 15:08:38 +0200

    Update team_stat.py

--- pyhtsl/stats/team_stat.py
+++ pyhtsl/stats/team_stat.py
@@ -36,7 +36,9 @@ class TeamStat(Stat):
         value = super()._as_string_second(include_fallback_value=include_fallback_value)
         if self.team is not None or value:
             name = self.team.name if isinstance(self.team, Team) else 'None'
-            return f' {name}"{value}'  # no quotes because htsl will break :( i need htsw
+            if ' ' in name:
+                name = f'"{name}"'  # this will break htsl :( waiting on htsw :D
+            return f' {name}{value}'
         return value
 
     def _equals(self, other: Checkable | HousingType) -> bool:
