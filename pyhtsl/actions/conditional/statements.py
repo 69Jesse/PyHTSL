687c665e7084ff2440552406d224144a6d2d0e24
Jesse <jesse@jessejanssen.nl>
2026-03-01 04:22:07 +0100

    Update statements.py

--- pyhtsl/actions/conditional/statements.py
+++ pyhtsl/actions/conditional/statements.py
@@ -18,8 +18,16 @@ def IfAnd(*conditions: Condition) -> IfContextManager:
     return IfContextManager(list(conditions), mode=ConditionalMode.AND)
 
 
-def IfOr(*conditions: Condition) -> IfContextManager:
-    return IfContextManager(list(conditions), mode=ConditionalMode.OR)
+def IfOr(
+    *conditions: Condition,
+    and_if_or_does_nothing: bool = True,
+) -> IfContextManager:
+    return IfContextManager(
+        list(conditions),
+        mode=ConditionalMode.OR
+        if len(conditions) > 1 or not and_if_or_does_nothing
+        else ConditionalMode.AND,
+    )
 
 
 Else: ElseContextManager = ElseContextManager()
