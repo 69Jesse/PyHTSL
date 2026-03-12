8d034ed9eff7f57c4c76f1f7e86373385a02aeb1
Jesse <jesse@jessejanssen.nl>
2026-03-12 16:02:20 +0100

    Update formatting.py

--- pyhtsl/utils/formatting.py
+++ pyhtsl/utils/formatting.py
@@ -19,22 +19,22 @@ def remove_formatting(text: str) -> str:
 
 
 COLOR_MAPPINGS: dict[str, int] = {
-    '§0': 0x000000,
-    '§1': 0x0000AA,
-    '§2': 0x00AA00,
-    '§3': 0x00AAAA,
-    '§4': 0xAA0000,
-    '§5': 0xAA00AA,
-    '§6': 0xFFAA00,
-    '§7': 0xAAAAAA,
-    '§8': 0x555555,
-    '§9': 0x5555FF,
-    '§a': 0x55FF55,
-    '§b': 0x55FFFF,
-    '§c': 0xFF5555,
-    '§d': 0xFF55FF,
-    '§e': 0xFFFF55,
-    '§f': 0xFFFFFF,
+    '0': 0x000000,
+    '1': 0x0000AA,
+    '2': 0x00AA00,
+    '3': 0x00AAAA,
+    '4': 0xAA0000,
+    '5': 0xAA00AA,
+    '6': 0xFFAA00,
+    '7': 0xAAAAAA,
+    '8': 0x555555,
+    '9': 0x5555FF,
+    'a': 0x55FF55,
+    'b': 0x55FFFF,
+    'c': 0xFF5555,
+    'd': 0xFF55FF,
+    'e': 0xFFFF55,
+    'f': 0xFFFFFF,
 }
 
 
@@ -54,8 +54,8 @@ def formatting_to_ansi(text: str) -> str:
     text = normalize_formatting(text)
 
     def replace(match: re.Match) -> str:
-        key = match.group(0)
-        if key == '§r':
+        key = match.group(1)
+        if key == 'r':
             return '\033[0m'
         color = COLOR_MAPPINGS.get(key, None)
         if color is None:
