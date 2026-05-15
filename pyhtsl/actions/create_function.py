df4ebcea2e8e9f572d35ca2fd228a4845755059b
Jesse Janssen <jesse@jessejanssen.nl>
2026-05-15 23:37:43 +0200

    Remove force_create from create_function

--- pyhtsl/actions/create_function.py
+++ pyhtsl/actions/create_function.py
@@ -10,19 +10,9 @@ __all__ = ('create_function',)
 def create_function(
     name: str,
     *,
-    force_create: bool | None = None,
     run_right_now: bool = False,
 ) -> Callable[[Callable[[], None]], Function]:
-    def decorator(func: Callable[[], None]) -> Function:
-        def callback() -> None:
-            create = (
-                force_create
-                if force_create is not None
-                else (func.__module__ == '__main__')
-            ) or (not get_current_container().is_global)
-            if create:
-                func()
-
+    def decorator(callback: Callable[[], None]) -> Function:
         function = Function(name=name)
         block = FunctionBlock(function=function, callback=callback)
 
